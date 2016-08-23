9802caae982dd1e94c4f7d1af380fca5383d87f4
inghamn <inghamn@bloomington.in.gov>
2016-08-23 14:05:13 -0400

    Fixed closure for async callbacks

--- public/js/cards/list.js
+++ public/js/cards/list.js
@@ -10,25 +10,27 @@ var CARD_LIST = {
             card = CARD_LIST.cards[i];
             id   = card.getAttribute('id');
 
-            DASHBOARD.ajax(card.getAttribute('data-queryUrl'), function (r) {
-                var data       = JSON.parse(r.responseText),
-                    target     = card.getAttribute('data-target'),
-                    comparison = card.getAttribute('data-comparison'),
-                    status     = 'fail',
-                    element    = document.getElementById(id);
+            (function (x) {
+                DASHBOARD.ajax(card.getAttribute('data-queryUrl'), function (r) {
+                    var element    = document.getElementById(x),
+                        data       = JSON.parse(r.responseText),
+                        value      = parseInt(data.value),
+                        target     = parseInt(element.getAttribute('data-target')),
+                        comparison = element.getAttribute('data-comparison'),
+                        status     = 'fail';
 
-                switch (comparison) {
-                    case 'gt' : if (data.value >  target) { status = 'pass'; } break;
-                    case 'gte': if (data.value >= target) { status = 'pass'; } break;
-                    case 'lt' : if (data.value <  target) { status = 'pass'; } break;
-                    case 'lte': if (data.value <= target) { status = 'pass'; } break;
-                }
-
-                if (data.value) {
-                    element.setAttribute('class', 'card ' + status);
-                    element.querySelector('.value').innerHTML = data.value;
-                }
-            });
+                    if (data.value) {
+                        switch (comparison) {
+                            case 'gt' : if (value >  target) { status = 'pass'; } break;
+                            case 'gte': if (value >= target) { status = 'pass'; } break;
+                            case 'lt' : if (value <  target) { status = 'pass'; } break;
+                            case 'lte': if (value <= target) { status = 'pass'; } break;
+                        }
+                        element.setAttribute('class', 'card ' + status);
+                        element.querySelector('.value').innerHTML = value;
+                    }
+                });
+            })(id);
         }
     }
 }
