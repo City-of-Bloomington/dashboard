a197323d21b8ca6114d31ded47634697527276d1
inghamn <inghamn@bloomington.in.gov>
2016-11-04 15:08:50 -0400

    Overhauled the ansible install script
    This script should work, in theory.  However, in practice it will not.  A development build must install dependencies from composer, which will be blocked, unless you set up private key authentication with Composer and Github.  This has to be done per-host, which is really not tenable.

--- ansible/roles/cob.dashboard/templates/createPerson.php
+++ ansible/roles/cob.dashboard/templates/createPerson.php
@@ -1,18 +1,16 @@
 <?php
 /**
- * @copyright 2012-2013 City of Bloomington, Indiana
+ * @copyright 2012-2016 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
- * @author Cliff Ingham <inghamn@bloomington.in.gov>
  */
 use Application\Models\Person;
 
 include '../bootstrap.inc';
 $person = new Person();
-$person->setFirstname('{{dashboard_user_fname}}');
-$person->setLastname('{{dashboard_user_lname}}');
-$person->setEmail('{{dashboard_user_email}}');
-
-$person->setUsername("{{ ansible_ssh_user }}");
+$person->setFirstname('{{ dashboard_admin.firstname }}');
+$person->setLastname ('{{ dashboard_admin.lastname  }}');
+$person->setEmail    ('{{ dashboard_admin.email     }}');
+$person->setUsername ('{{ dashboard_admin.username  }}');
 //$person->setPassword();
 $person->setAuthenticationMethod('Employee');
 $person->setRole('Administrator');
