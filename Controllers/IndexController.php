ead05fac4918eea445ca73d8a60bc29ad4d27c87
inghamn <inghamn@bloomington.in.gov>
2016-11-29 13:57:06 -0500

    Added group selection to the navbar
    The group icons are now defined in the theme.  When creating groups, you only specify what CSS class you would like to use.  It's up to the theme to put in the icons...or not.
    
    Fixes #5

--- Controllers/IndexController.php
+++ Controllers/IndexController.php
@@ -6,14 +6,25 @@
 namespace Application\Controllers;
 
 use Application\Models\CardsTable;
+use Application\Models\Group;
+
 use Blossom\Classes\Controller;
 
 class IndexController extends Controller
 {
 	public function index(array $params)
 	{
+        if (!empty($_GET['group_id'])) {
+            try {
+                $group = new Group($_GET['group_id']);
+            }
+            catch (\Exception $e) { }
+        }
+
+        $search = isset($group) ? ['group_id' => $group->getId()] : null;
+
         $table = new CardsTable();
-        $list  = $table->find();
+        $list  = $table->find($search);
         return new \Application\Views\Cards\DashboardView(['cards'=>$list]);
 	}
 }
